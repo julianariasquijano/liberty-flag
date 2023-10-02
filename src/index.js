400ff9e75f16e674e4272dcbc875cdfa64aba6e8
Proximus <user@proximustech.net>
2023-10-01 20:10:13 -0500

    Add render view

--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@
 
 const Koa = require('koa');
 const Router = require('@koa/router');
+const render = require('koa-ejs');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('koa-example', '/', (ctx) => {
-  ctx.body = 'Hello World';
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'index',
+  viewExt: 'html',
+  cache: false,
+  debug: true
 });
+
+router.get('index', '/', (ctx) => {
+  let featureFlags = [];
+
+  featureFlags.push({
+    meta_name: 'light-theme',
+    meta_value: '0'
+  });
+
+  featureFlags.push({
+    meta_name: 'authorization',
+    meta_value: '1'
+  });
+
+  return ctx.render('index', {
+    featureFlags: featureFlags
+  });  
+
+})
 
 app
   .use(router.routes())
